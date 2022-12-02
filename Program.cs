31610f9863f651031c47e9de353e9de82a331bec
MIkhail <edvard_kenuey_10@mail.ru>
2022-12-03 02:47:54 +0300

    решение второго с любыми значениями

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿int NumberA = 2;
+int NumberB = 3;
+int NumberC = 7;
+if (NumberA < NumberB)
+{
+    if (NumberB < NumberC)
+    {
+        Console.WriteLine(NumberC);
+    }
+    if (NumberB > NumberC)
+    {
+        Console.WriteLine(NumberB);
+    }
+    if (NumberB == NumberC)
+    {
+        Console.WriteLine("числа равные");
+    }
+}
+if (NumberA == NumberB)
+{
+    if (NumberB > NumberC)
+    {
+        Console.WriteLine(NumberB);
+    }
+    if (NumberB < NumberC)
+    {
+        Console.WriteLine(NumberC);
+    }
+    if (NumberB == NumberC )
+    {
+        Console.WriteLine("числа равные");
+    }
+}
+if (NumberA > NumberB)
+{
+    if (NumberA < NumberC)
+    {
+        Console.WriteLine(NumberC);
+    }
+    if (NumberA > NumberC)
+    {
+        Console.WriteLine(NumberA);
+    }
+    if (NumberA == NumberC )
+    {
+        Console.WriteLine("числа равные");
+    }
+}
