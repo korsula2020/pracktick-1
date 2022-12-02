405e0018846d3a2e8bc625522c2166672ef82164
MIkhail <edvard_kenuey_10@mail.ru>
2022-12-03 02:48:46 +0300

    решение первого с любыми переменными

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double NumberA = -1;
+double NumberB = -6;
+if (NumberA < NumberB)
+{
+    Console.WriteLine(NumberB);
+}
+if (NumberA == NumberB)
+{
+    Console.WriteLine("числа равны");
+}
+if (NumberA > NumberB)
+{
+    Console.WriteLine(NumberA);
+}
